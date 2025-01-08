a2ed127d68cacca307a07946542eb58707e5125c
wujun <wujun728@163.com>
2025-01-08 17:32:29 +0800

    fix： 调整依赖

--- jun_springboot_vue/jun_admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ jun_springboot_vue/jun_admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -1,11 +1,11 @@
 package com.ruoyi;
 
-import cn.hutool.core.util.ObjectUtil;
 import com.ruoyi.common.utils.spring.SpringUtils;
 import com.ruoyi.qixing.controller.QixingController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.util.ObjectUtils;
 
 /**
  * 启动程序
@@ -21,8 +21,8 @@ public class RuoYiApplication
         SpringApplication.run(RuoYiApplication.class, args);
         System.out.println("(♥◠‿◠)ﾉﾞ  若依启动成功   ლ(´ڡ`ლ)ﾞ  \n");
         QixingController qx = SpringUtils.getBean(QixingController.class);
-        if(ObjectUtil.isEmpty(qx)){
-            System.out.println(" qixing is null ");
+        if(!ObjectUtils.isEmpty(qx)){
+            System.out.println(" qixing is not null ");
         }
     }
 }
--- jun_springboot_vue/jun_qixing/src/main/java/com/ruoyi/qixing/controller/QixingController.java
+++ jun_springboot_vue/jun_qixing/src/main/java/com/ruoyi/qixing/controller/QixingController.java
@@ -1,16 +1,15 @@
 package com.ruoyi.qixing.controller;
 
 import cn.hutool.core.util.ObjUtil;
-import com.jfinal.plugin.activerecord.Db;
-import com.jfinal.plugin.activerecord.Record;
 import com.ruoyi.common.core.controller.BaseController;
 import com.ruoyi.common.core.domain.AjaxResult;
 import com.ruoyi.common.utils.SecurityUtils;
 import com.ruoyi.common.utils.spring.SpringUtils;
 import com.ruoyi.system.service.ISysMenuService;
-import io.github.wujun728.db.ActiveRecordUtil;
-import io.github.wujun728.db.RecordUtil;
-import io.github.wujun728.db.TreeBuildUtil;
+import io.github.wujun728.db.record.Db;
+import io.github.wujun728.db.record.Record;
+import io.github.wujun728.db.utils.RecordUtil;
+import io.github.wujun728.db.utils.TreeBuildUtil;
 import org.apache.commons.compress.utils.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,7 +24,6 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
- *
  * @author wujun
  */
 @RestController
@@ -59,17 +57,18 @@ public class QixingController extends BaseController
         left join sys_user u on ur.user_id = u.user_id
         where u.user_id = #{userId} and m.menu_type in ('M', 'C') and m.status = 0  AND ro.status = 0
         order by m.parent_id, m.order_num*/
-        ActiveRecordUtil.initActiveRecordPlugin("main", SpringUtils.getBean(DataSource.class));
+        //ActiveRecordUtil.initActiveRecordPlugin("main", SpringUtils.getBean(DataSource.class));
+        Db.init("main", SpringUtils.getBean(DataSource.class));
         List<Record> apps = Db.use().find(" SELECT * from sys_menu where parent_id = 0 ");
         List apps1 = RecordUtil.recordToMaps(apps,true);
         List<Record> menus = Db.use().find(" SELECT * from sys_menu where menu_type != 'F' ORDER BY parent_id,order_num ");
-        List<Map<String, Object>> menus1 = RecordUtil.recordToMaps(menus,true);
+        List<Map> menus1 = RecordUtil.recordToMaps(menus,true);
         for(Map map : menus1){
             if(ObjUtil.isNotEmpty(map.get("component"))){
                 map.put("component",map.get("component")+".html");
             }
         }
-        menus1 = (List) menus1.stream().map(item-> item).collect(Collectors.toList());
+        menus1 = (List) menus1.stream().map(item->{return item;}).collect(Collectors.toList());
         List menus2 = TreeBuildUtil.listToTree(menus1,"0","menu_id","parent_id");
         Map data = new HashMap<>();
         data.put("apps",apps1);
