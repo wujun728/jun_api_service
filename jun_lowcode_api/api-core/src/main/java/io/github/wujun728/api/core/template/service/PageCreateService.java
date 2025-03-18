f28ed8311b606bc93c8303e33ca8619d8224a844
wujun <wujun728@163.com>
2025-03-18 14:47:25 +0800

    1

--- jun_lowcode_api/api-core/src/main/java/io/github/wujun728/api/core/template/service/PageCreateService.java
+++
@@ -1,8 +0,0 @@
-package io.github.wujun728.api.core.template.service;
-
-import io.github.wujun728.api.core.entity.ApiInfo;
-
-public interface PageCreateService {
-
-    public void createFile(ApiInfo apiInfo);
-}
